f6a2e0d42b3d7cd3e3a0d7c89ef1eb90f13392d3
Adam Rosenbaum <adam.rosenbaum@scilifelab.se>
2018-09-13 11:02:24 +0200

    CHANGES TO PULL REQUEST
    ...

--- mutacc/cli/importing.py
+++ mutacc/cli/importing.py
@@ -1,9 +1,6 @@
-import json
-import sys, os
-
 import click
 
-from mutacc.utils.hk_call import find_files
+from mutacc.utils.json_parse import parse_json
 
 @click.command('import')
 @click.option('--bam', multiple = True, type = click.Path(exists = True), help = 'path to .bam file')
@@ -11,14 +8,14 @@ from mutacc.utils.hk_call import find_files
 @click.option('--variant', help = 'Variant data as JSON string, or path to .json file')
 @click.option('--case', help = 'Metadata for the case as JSON string or path to .json file')
 @click.pass_context
-def importing(context,bam,fastq,variant,case):
+def importing(context, bam, fastq, variant, case):
 
     """
         Import cases to the database
     """
             
-    case = parse_JSON(case)
-    variant = parse_JSON(variant)
+    case = parse_json(case)
+    variant = parse_json(variant)
 
     for path in bam:
         click.echo(path)
@@ -26,31 +23,3 @@ def importing(context,bam,fastq,variant,case):
     click.echo(case)
     click.echo(variant)
 
-def parse_JSON(json_format):
-
-    if json_format[0] == '~':
-
-        json_format = os.path.expanduser(json_format)
-
-    if os.path.isfile(json_format):
-
-        with open(json_format, 'r') as json_handle:
-                
-            try: 
-                json_format = json.load(json_handle)
-
-            except json.JSONDecodeError:
-                    
-                sys.exit("Not valid Json")
-
-    else:
-
-        try: 
-
-            json_format = json.loads(json_format)
-
-        except json.JSONDecodeError:
-                    
-            sys.exit("Not valid Json")
-
-    return json_format
