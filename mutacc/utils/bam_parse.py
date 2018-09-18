35b1511e15a1bfc68e126904a35977255c1bf3b3
Adam Rosenbaum <adam.rosenbaum@scilifelab.se>
2018-09-18 16:24:18 +0200

    Get read name from bam and find read in fastq
    mutacc/utils/bam_parse contains a simple function that get read names
    from bam file. mutacc/utils/fastq_parse contains function to find list of record
    IDs and write only them to new fastq files. Have added additional
    comments in fastq_parse since first pull request

--- mutacc/utils/bam_parse.py
+++ mutacc/utils/bam_parse.py
@@ -2,34 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import pysam
+from mutacc.utils.fastq_parse import fastq_extract
 
 def get_overlaping_reads(fileName, chrom, start, end, padding = 300):
-"""
+    """
 
-Extracts all read names from a bam file, overlapping the specified region.
+        Extracts all read names from a bam file, overlapping the specified region.
 
-Args:
+        Args:
 
-    fileName (string): name of bam file
-    chrom (string): name of chromosome
-    start (int): start of region
-    end (int): end of region
-    padding (int): optional padding of region. i.e. Reads in the vicinity vill also be detected
-    depending on the padding parameter. 
+            fileName (string): name of bam file
+            chrom (string): name of chromosome
+            start (int): start of region
+            end (int): end of region
+            padding (int): optional padding of region. i.e. Reads in the vicinity vill also be detected
+                           depending on the padding parameter. 
 
-Returns:
+        Returns:
 
-    reads (list): List of read identifiers. 
-    
-"""
+            reads (set): set of read names. 
+        
+    """
     sam_file = pysam.AlignmentFile(fileName, 'rb')
         
     reads = sam_file.fetch(reference = chrom, 
             start = start - padding, 
             end = end + padding)
     
-    return [read.query_name for read in reads]
+    ids = [read.query_name for read in reads]
 
-
+    return set(ids)
      
-    
--- mutacc/utils/fastq_parse.py
+++ mutacc/utils/fastq_parse.py
@@ -4,7 +4,7 @@ from contextlib import ExitStack
 
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
 
-def fastq_extract(fastq_files: list, record_ids: list, dir_path = ''):
+def fastq_extract(fastq_files: list, record_ids: set, dir_path = ''):
 
     """
 
@@ -14,7 +14,7 @@ def fastq_extract(fastq_files: list, record_ids: list, dir_path = ''):
         Args:
             
             fastq_files (list): List of fastqfiles
-            record_ids (list): List of read identifiers
+            record_ids (set): Set of read names
             dir_path (string): path to directory where new fastq files are written to
         
         Returns:
@@ -29,39 +29,54 @@ def fastq_extract(fastq_files: list, record_ids: list, dir_path = ''):
 
     if not dir_path.exists():
         raise FileNotFoundError('path not found:  %s' % (dir_path))
-
+    
+    #Uses ExitStack context manager to manage a variable number of
+    #files
     with ExitStack() as stack:
-        #Opens fastq files and places file handles in list fastq_handles
+        #Opens fastq files and places file handles in list fastq_handles and Opens __exit__ method 
+        # to the ExitStack callback stack.
         fastq_handles = [stack.enter_context(get_file_handle(fastq_file)) \
                 for fastq_file in fastq_files]
         
-        #Opens fastq files to write
+        #Opens fastq files to write found records.
         out_handles = [stack.enter_context(open(dir_path.joinpath('ex_' + file_name), 'wt')) \
                 for file_name in file_names]
 
         #parse fastq and places in list fastqs
+        #FastqGeneralIterator parses each record as a tuple with name, seq, and quality
+        #on index 0, 1, 3 respectively.
         fastqs = [FastqGeneralIterator(handle) for handle in fastq_handles]
 
         #Iterates over parsed fastq files simultaneously
         for records in zip(*fastqs):
-
+            
+            #Checks if current record name exists in record_ids. This Check is only done for one of the
+            #fastq files (records[0]). It is thus assumed that paired end reads exists on the same
+            #position in the two files
+            #Example: if records[0][0] is 'ST-E00266:38:H2TF5CCXX:8:1101:2563:2170 1:N:0:CGCGCATT',
+            # records[0][0].split()[0] is 'ST-E00266:38:H2TF5CCXX:8:1101:2563:2170'
             if records[0][0].split()[0] in record_ids:
-                
+
+                #Writes current records from each fastq file to corresponding output file
                 for record, out_handle in zip(records, out_handles):
 
                     out_handle.write("@%s\n%s\n+\n%s\n" % (record[0], record[1], record[2]))
-
+                
+                #removes found record name from the record_ids set
                 record_ids.remove(records[0][0].split()[0])
-
+                
+                #If record_ids is empty all records have been found so there is no need to iterate
+                #further over the fastq files
                 if len(record_ids) == 0:
 
                     break
-        
+
+    #Returns the file paths for the output fastq files, that should only contain the records
+    #with its record name in record_ids
         out_paths = [out_handle.name for out_handle in out_handles]
 
     return out_paths
 
-      
 
 def get_file_handle(file_name):
     
