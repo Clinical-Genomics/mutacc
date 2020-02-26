a98d353638253422209b0f92052e6599b155d7d8
Adam Rosenbaum <adam.rosenbaum@scilifelab.se>
2020-02-26 13:37:51 +0100

    Refactoring of code

--- mutacc/utils/bam_handler.py
+++ mutacc/utils/bam_handler.py
@@ -9,7 +9,6 @@ import os
 import pysam
 
 from mutacc.parse.path_parse import parse_path
-from mutacc.utils.constants import PADDING
 
 LOG = logging.getLogger(__name__)
 
@@ -19,7 +18,7 @@ class BAMContext:
         Context manager to deal with bam files
     """
 
-    def __init__(self, bam_file, out_dir=None, paired=True):
+    def __init__(self, bam_file, out_dir=None):
         """
             Args:
                 bam_file (str): path to bam file
@@ -29,8 +28,9 @@ class BAMContext:
         self.bam_file = parse_path(bam_file)
         self.file_name = self.bam_file.name
         self.bam = pysam.AlignmentFile(self.bam_file, "rb")
+        self.paired, self.length, self.insert_size = self._check_bam()
         self.reads = {}
-        self.ends = 2 if paired else 1
+        self.ends = 2 if self.paired else 1
         self.found_reads = set()
         self.out_dir = out_dir
         if self.out_dir:
@@ -39,10 +39,10 @@ class BAMContext:
             self.out_bam = pysam.AlignmentFile(self.out_name, "wb", template=self.bam)
 
     def __enter__(self):
-
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """Closes file handles and deletes tnp files"""
 
         self.bam.close()
 
@@ -54,19 +54,22 @@ class BAMContext:
         except AttributeError:
             pass
 
-    def find_read_names_from_region(self, chrom, start, end):
+    def find_read_names_from_region(self, chrom, start, end, padding=None):
         """Adds the read names in region to the set found_reads
 
             Args:
                 chrom (str): name of contig
                 start (int): start of region
                 end (int): end of region
+                padding (int): padding around region
         """
 
-        read_names = [read.query_name for read in self.bam.fetch(chrom, start, end)]
+        adjusted_padding = self._adjust_padding(padding)
+
+        read_names = [read.query_name for read in self.bam.fetch(chrom, start-adjusted_padding, end+adjusted_padding)]
         self.found_reads = self.found_reads.union(set(read_names))
 
-    def find_reads_from_region(self, chrom, start, end, brute=False, find_mates=True):
+    def find_reads_from_region(self, chrom, start, end, brute=False, find_mates=True, padding=None):
 
         """
             Writes the found reads in region to a new bam-file, if a out-dir is
@@ -82,7 +85,8 @@ class BAMContext:
                 find_mates (bool): If True, tries to find the mates of unmatched reads
 
         """
-        for read in self.bam.fetch(chrom, start, end):
+        adjusted_padding = self._adjust_padding(padding)
+        for read in self.bam.fetch(chrom, start-adjusted_padding, end+adjusted_padding):
             read_name = read.query_name
             if read_name in self.found_reads:
                 continue
@@ -198,84 +202,75 @@ class BAMContext:
         return self.names_temp
 
 
-def check_bam(bam_file):
+    def _check_bam(self):
 
-    """Checks if reads in bam are paired
-    Args:
-        bam_file (path): path to bam file
+        """Checks if reads in bam are paired
+        Args:
+            bam_file (path): path to bam file
 
-    Returns:
-        paired (bool): True if reads are paired
-    """
-    paired = False
-    with pysam.AlignmentFile(bam_file) as bam:
+        Returns:
+            paired (bool): True if reads are paired
+        """
+
+        insert_sizes = []
+        acc_length = 0
+        paired = False
         count = 0
-        for read in bam:
+        no_reads = 10000
+        for read in self.bam():
             count += 1
             if read.is_paired:
                 paired = True
-                break
-            if count == 1000:
-                break
-
-    return paired
-
-
-def get_length(bam_file):
-    """Calculate the average read length from first 1000 reads in bam
-    Args:
-        bam_file (path): path to bam file
-    Returns:
-        average_length (int): average read length
-    """
-    acc_length = 0
-
-    with pysam.AlignmentFile(bam_file) as bam:
-        count = 0
-        for read in bam:
-            count += 1
             acc_length += read.query_length
-            if count == 1000:
+            insert_sizes.append(read.template_length)
+            if count == no_reads:
                 break
-
-    average_length = acc_length / 1000
-
-    return average_length
-
-
-def get_real_padding(length, padding=None):
-    """Given the read lengths in a BAM, calculate the real padding needed taking
-    into consideration the read length. This should hopefully approximate
-    the PADDING constant.
-    Args:
-        length (int): read length
-    Returns:
-        real_padding (int): sample specific padding to approximate PADDING
-
-    """
-    padding = padding or PADDING
-    real_padding = padding - length / 2
-    if real_padding < 1:
-        real_padding = 1
-
-    return real_padding
+        mean_length = acc_length / no_reads
+        insert_sizes.sort()
+        median_insert_size = insert_sizes[no_reads/2]
 
 
-def get_overlaping_reads(file_name, chrom, start, end):
-    """Extracts all read names from a bam file, overlapping the specified region.
+        return paired, mean_length, median_insert_size
 
+
+    def get_length(bam_file):
+        """Calculate the average read length from first 1000 reads in bam
         Args:
-            file_name (string): name of bam file
-            chrom (string): name of chromosome
-            start (int): start of region
-            end (int): end of region
-
+            bam_file (path): path to bam file
         Returns:
-            reads (set): set of read names.
-    """
-    file_name = parse_path(file_name)
-    bam_file = pysam.AlignmentFile(file_name, "rb")
-    reads = bam_file.fetch(reference=chrom, start=start, end=end)
-    ids = [read.query_name for read in reads]
-    bam_file.close()
-    return set(ids)
+            average_length (int): average read length
+        """
+        acc_length = 0
+        acc_insert_size = 0
+
+        with pysam.AlignmentFile(bam_file) as bam:
+            count = 0
+            for read in bam:
+                count += 1
+                acc_length += read.query_length
+                acc_insert_size += read.temlate_length
+                if count == 1000:
+                    break
+
+        average_length = acc_length / 1000
+
+        return average_length
+
+
+    def _adjust_padding(self, padding):
+        """Given the read lengths in a BAM, calculate the real padding needed taking
+        into consideration the read length. This should hopefully approximate
+        the PADDING constant.
+        Args:
+            length (int): read length
+        Returns:
+            real_padding (int): sample specific padding to approximate PADDING
+
+        """
+        if padding is None:
+            padding = 0
+        adjusted_padding = padding - self.length / 2
+        if adjusted_padding < 0:
+            adjusted_padding = 0
+
+        return adjusted_padding
