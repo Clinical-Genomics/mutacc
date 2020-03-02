d9f151571eee3a760a7c086c09cfed2615a06ce5
Adam Rosenbaum <adam.rosenbaum@scilifelab.se>
2020-03-02 10:00:39 +0100

    Add test

--- tests/utils/test_bam_handler.py
+++ tests/utils/test_bam_handler.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from mutacc.utils.bam_handler import BAMContext
 
 
-def test_BAMContext(tmpdir, read_ids_fixed):
+def test_BAMContext(tmpdir):
 
     with BAMContext(
         bam_file = "tests/fixtures/reduced_ref_4_1000000_10002000.bam",
@@ -26,4 +26,23 @@ def test_BAMContext(tmpdir, read_ids_fixed):
 
         assert Path(temp_names).exists()
 
+
     assert not Path(temp_names).exists()
+
+
+def test_BAMContext_find_read_names(read_ids_fixed):
+
+    # GIVEN a list of reads names that are in the bam-file
+    with BAMContext(
+        bam_file = "tests/fixtures/reduced_ref_4_1000000_10002000.bam",
+        ) as bam_handle:
+
+    # WHEN extracting the reads names from region
+        bam_handle.find_read_names_from_region(
+            chrom = "4",
+            start = 1000000,
+            end = 10002000
+        )
+
+    #THEN the set of read names found should correspond to the the above read names
+        assert set(read_ids_fixed) == bam_handle.found_reads
